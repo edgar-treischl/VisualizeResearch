b5eb8fd8a90ad0d229e94dbec1c4f4f5851681e2
Edgar Treischl <77931249+edgar-treischl@users.noreply.github.com>
2021-11-20 11:00:50 +0100

    20/11/2021

---
+++ R/Leaky_Pipeline.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+
+leaky_pipeline <- tibble::tribble(
+                  ~Gruppe, ~`2017`, ~`2018`, ~`2019`,     ~Sex,
+            "Studierende",     485,     489,     493, "Frauen",
+            "Absolventen",     508,     511,     517, "Frauen",
+            "Promotion",     448,     452,     454, "Frauen",
+         "Habilitation",     293,     316,     319, "Frauen",
+         "C4-Professur",     115,     117,     117, "Frauen",
+            "Studierende",     515,     511,     507, "Männer",
+            "Absolventen",     492,     489,     483, "Männer",
+            "Promotion",     552,     548,     546, "Männer",
+         "Habilitation",     707,     684,     681, "Männer",
+         "C4-Professur",     885,     883,     883, "Männer"
+        )
+
+leaky_pipeline$Gruppe <- factor(leaky_pipeline$Gruppe, 
+                                levels = c("Studienanfänger", 
+                                           "Studierende",
+                                           "Absolventen", 
+                                           "Promotion", 
+                                           "Habilitation", 
+                                           "C4-Professur"))
+
+
+
+
+ggplot(leaky_pipeline, aes(x=Gruppe, y=`2019`, group = Sex, color = Sex)) +
+  geom_line(size = 1) +
+  geom_point(size = 2)+
+  theme_minimal(base_size = 18)+
+  geom_label(data = leaky_pipeline %>% filter(Gruppe == "Habilitation"),
+             aes(label = Sex, color = Sex),
+             #hjust = 1.0,
+             #vjust = 0.5,
+             fontface = "bold",
+             size = 5.5)+
+  labs(caption = "Daten: destatis.de",
+       y = "Anteile nach akademischer Laufbahn (2019)")+
+  theme(legend.position="none")+
+  scale_color_manual(values = c("#264653", "#e76f51"))
+
+
+#Export
+ggsave(file="Leaky_Pipeline.png", width = 210, height = 148, units = "mm")
+
--- README.md
+++ README.md
@@ -19,7 +19,7 @@ plots.
 
 Engelhardt & Lörz (2021) diskutieren die Auswirkungen von Studienkosten
 auf herkunftsspezifische Ungleichheiten und vergleichen dabei wie sich
-die Befög Reform auf die Studienaufnahme und der Studienfachwahl
+die Bafög Reform auf die Studienaufnahme und der Studienfachwahl
 auswirkt.
 
 Das folgende Liniendiagramm zeigt, wie sich die Anzahl an Studierenden
@@ -34,3 +34,18 @@ entwickelt hat und visualisiert Tabelle 1 in Engelhardt & Lörz (2021).
 Studienkosten auf herkunftsspezifische Ungleichheiten bei der
 Studienaufnahme und der Studienfachwahl, Köln Z Soziol, 73, S. 285–305.
 DOI: <https://doi.org/10.1007/s11577-021-00787-3>
+
+## Leaky Pipeline
+
+Die Leaky Pipeline zeigt, dass ein Löwenanteil der Studierenden in
+Deutschland zwar weiblich ist, der Bildungserfolg sich aber nicht in der
+akademischen Karriere niederschlägt. Vielmehr gehen die Anteile von
+Männer und Frauen auseinander.
+
+![The Leaky Pipeline](man/figures/Leaky_Pipeline.png)
+
+[*Quellcode*](https://github.com/edgar-treischl/VisualizeResearch/blob/master/R/Engelhardt_L%C3%B6rz_2021.R)
+
+*Beispielpublikation*: Schubert, F. & Engelange, S. (2011): Wie undicht
+ist die Pipeline? Wissenschaftskarrieren von promovierten Frauen, Kölner
+Zeitschrift für Soziologie und Sozialpsychologie, 63 (431).
